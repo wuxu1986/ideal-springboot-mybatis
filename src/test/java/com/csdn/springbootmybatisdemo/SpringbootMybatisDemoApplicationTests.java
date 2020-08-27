b993d9034fc62d4096a64312a2508cb9b4d9e9c2
wuxu <174511373@qq.com>
2020-08-27 15:48:14 +0800

    applicationtest

--- src/test/java/com/csdn/springbootmybatisdemo/SpringbootMybatisDemoApplicationTests.java
+++ src/test/java/com/csdn/springbootmybatisdemo/SpringbootMybatisDemoApplicationTests.java
@@ -3,11 +3,17 @@ package com.csdn.springbootmybatisdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.security.PrivateKey;
+
 @SpringBootTest
 class SpringbootMybatisDemoApplicationTests {
 
     @Test
     void contextLoads() {
+        //本地地址ideal哈哈
+        String name;
+        String password;
+
     }
 
 }
